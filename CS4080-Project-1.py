d5905838c7c4d059b8a2cc1d4b20699d4eee3ef0
kaylee-barcroft <kaylee.barcroft@gmail.com>
2025-02-18 17:16:54 -0700

    randomized nodes in Falcon to test with. Solved with Dijkstra

--- CS4080-Project-1.py
+++ CS4080-Project-1.py
@@ -4,6 +4,7 @@
 
 import osmnx as ox
 import networkx as nx
+import random
 
 #Small sample plot: Falcon
 
@@ -13,8 +14,8 @@ place_name = "Falcon, Colorado, USA"
 # Download the road network for driving
 G = ox.graph_from_place(place_name, network_type="drive")
 
-# Visualize the graph
-ox.plot_graph(G)
+# Visualize the graph (before shortest path calculations)
+#ox.plot_graph(G)
 
 print("Sample nodes and attached data:")
 for node, data in list(G.nodes(data=True))[:10]:
@@ -22,9 +23,14 @@ for node, data in list(G.nodes(data=True))[:10]:
 for u, v, data in list(G.edges(data=True))[:10]:
     print(u, v, data)
 
-orig, dest = list(G.nodes)[:2]  # Choose two nodes
-path = nx.shortest_path(G, source=orig, target=dest, weight="length")
-print("Shortest path:", path)
+#orig, dest = list(G.nodes)[:2]  # Predetermined 2 nodes
+orig, dest = random.choices(list(G.nodes), k=2)  # Choose two nodes randomly
+
+# Compute the shortest path using Dijkstra's algorithm (built in)
+route = nx.shortest_path(G, source=orig, target=dest, weight="length")
+
+# Plot the graph with the route highlighted
+ox.plot_graph_route(G, route, route_linewidth=4, route_color="red", node_size=30)
 
 '''Part 1: Implement CH with two different node sort methods...
     1: Node-Based
